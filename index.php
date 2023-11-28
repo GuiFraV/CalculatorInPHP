52bf56bfabe572c513fd0b570ce66eddc19b573a
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-28 10:29:26 +0100

    refacto separation code

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php  include "calculator_logic.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,60 +25,12 @@
         <button>Calculate</button>
 
     </form>
+    
 
     <?php 
-    
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
-            $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
-            $operator = htmlspecialchars($_POST["operator"]);
-
-            $errors = false;
-
-            if(empty($num01) || empty($num02) || empty($operator)){
-
-                echo "<p>Fill in all fields !</p>";
-                $errors = true;
-
-            }
-
-            if(!is_numeric($num01) || !is_numeric($num02)){
-
-                echo "<p>Only write numbers !</p>";
-                $errors = true;
-
-            }
-
-            if(!$errors){
-                $value = 0;
-
-                switch($operator){
-                    case "add":
-                        $value = $num01 + $num02;
-                        break;
-                    case "substract":
-                        $value = $num01 - $num02;
-                        break;
-                    case "multiply":
-                        $value = $num01 * $num02;
-                        break;
-                    case "divide":
-                        $value = $num01 / $num02;
-                        break;
-                    default:
-                        echo "<p>Something went wrong !</p>";
-                }
-
-                echo "<p class='calc-result'> Result = " . $value . "</p>";
-
-            }
-
-
-
-
+        if($value !== " "){
+            echo "<p class='calc-result'> Result = " . $value . "</p>";
         }
-    
-    
     ?>
     
 </body>
